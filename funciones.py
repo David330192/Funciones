483d0511b1eb545a4c780b5dc9e8e5682075e085
Jesus David P.T <jpupo5@estudiantes.areandina.edu.co>
2022-06-29 20:41:03 -0500

    Mi cuarta funcion

--- funciones.py
+++ funciones.py
@@ -1,8 +1,15 @@
 import os
 os.system("cls")
-def nombre(): # Mi primera funcion
+def suma(): # Mi primera funcion
     a=2
     b=8
     c=a+b
-    print(c)
-nombre()
+    print("El resultado de la suma es: ",c)
+suma()
+
+def resta ():
+    a=10
+    b=6
+    c=a-b
+    print("El resultado de la resta es:",c)
+resta()
